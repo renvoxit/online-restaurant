be9d9fd9141e4af41ef6d4483b125d2756aea9b2
kornix-dev <daria.bagaieva@gmail.com>
2025-11-11 18:52:26 +0100

    Configured environment for Render deployment

--- config.py
+++ config.py
@@ -3,9 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 import os
+# Configuration class for Flask application
 
 
 class Config:
+    # Replace with your actual database URL
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY", "dev_secret_key")
