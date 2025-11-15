31a1727b4a53cdcfebc39f47490815b9da6bcf2b
kornix-dev <daria.bagaieva@gmail.com>
2025-11-15 18:51:49 +0100

    fix

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
+from seed_menu import seed_menu
+from models.models import User, Dish
 from flask import Flask
 from config import Config
-from models.models import User
 from extensions import db, login_manager, csrf, migrate, limiter
 
 login_manager.login_view = 'auth.login'
 
 
+# Load user callback
 @login_manager.user_loader
 def load_user(user_id):
+    from models.models import User
     return User.query.get(int(user_id))
 
 
@@ -16,41 +19,23 @@ def create_app():
     app.config.from_object(Config)
     app.config.from_pyfile('config.py', silent=True)
 
+    # init extensions
     db.init_app(app)
     migrate.init_app(app, db)
     login_manager.init_app(app)
     csrf.init_app(app)
     limiter.init_app(app)
 
+    # register blueprints
     from routes.auth import auth_bp
     from routes.main import main_bp
     from routes.admin import admin_bp
+
     app.register_blueprint(auth_bp)
     app.register_blueprint(main_bp)
     app.register_blueprint(admin_bp)
 
-    with app.app_context():
-        db.create_all()
-
-    from models.models import User, Dish
-    from seed_menu import seed_menu
-
-    # create admin if missing
-    if not User.query.filter_by(username="admin").first():
-        admin = User(
-            username="admin",
-            email="admin@example.com",
-            is_admin=True
-        )
-        admin.set_password("admin")
-        db.session.add(admin)
-
-    # seed menu
-    if Dish.query.count() == 0:
-        seed_menu()
-
-    db.session.commit()
-
+    # security headers
     @app.after_request
     def set_headers(resp):
         resp.headers["Content-Security-Policy"] = (
@@ -62,6 +47,26 @@ def create_app():
 
 
 app = create_app()
+
+
+with app.app_context():
+    db.create_all()
+
+    # create admin if missing
+    if not User.query.filter_by(username="admin").first():
+        admin = User(
+            username="admin",
+            email="admin@example.com",
+            is_admin=True
+        )
+        admin.set_password("admin")
+        db.session.add(admin)
+
+    # seed menu if needed
+    if Dish.query.count() == 0:
+        seed_menu()
+
+    db.session.commit()
 
 if __name__ == '__main__':
     app.run(debug=True)
